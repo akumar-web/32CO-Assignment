138ce0a5b749403aa7c96e1e1c19f56215d82fc5
akumar-web <61603682+akumar-web@users.noreply.github.com>
2025-08-01 20:54:51 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
 
   backend "s3" {
-    bucket         = "demo-terraform-eks-state-s3-bucket"
+    bucket         = "eks1-terraform-eks-state-s3-bucket"
     key            = "terraform.tfstate"
     region         = "us-west-2"
     dynamodb_table = "terraform-eks-state-locks"
